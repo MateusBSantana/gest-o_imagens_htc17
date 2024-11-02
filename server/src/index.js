3850f2979eb8fdae42ee6b2b5f8d7b736e389516
MATEUS BARBOZA SANTANA <msantana@labcetec.net>
2024-11-01 21:23:01 -0300

    Criando CRUD - Sistema de armazenamento de imagens

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { downloadImagem, inserindoImagem } from './controllers/ImagemController.js';
+import fileupload from 'express-fileupload'
+
+const app = express();
+const porta = 5000;
+
+app.use(fileupload());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API Funcionando');
+});
+
+app.get('/public/:nome_imagem', downloadImagem);
+
+//CRUD Imagens
+app.post('/imagem', inserindoImagem);
+
+app.listen(porta, () => {
+    console.log(`API Rodando na porta ${porta}`);
+});
